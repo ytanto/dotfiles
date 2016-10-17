4e57e97af51124f5717de7201cebc2b4eabf0e26
mochi8k <y.hkh.185@gmail.com>
2016-10-18 00:04:10 +0900

    update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,6 +7,7 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
+;; ロードパス追加用関数
 (defun add-to-load-path (&rest paths)
   (mapc '(lambda (path)
            (add-to-list 'load-path path))
@@ -18,24 +19,37 @@
 
 ;;; 画像ファイルを表示
 (auto-image-file-mode t)
+
 ;;; メニューバーを消す
 (menu-bar-mode -1)1
+
 ;;; ツールバーを消す
 (tool-bar-mode -1)
+
 ;;; カーソルの点滅を止める
 (blink-cursor-mode 0)
+
 ;;; テーマの設定
 (load-theme 'misterioso t)
+
 ;;; Welcomeページを非表示
 (setq inhibit-startup-message t)
+
 ;; scratchの初期メッセージ消去
 (setq initial-scratch-message "")
+
+;; 自動保存を無効
+(setq auto-save-default nil)
+
 ;; タイトルバーにファイルのフルパス表示
 (setq frame-title-format "%f")
+
 ;; タブをスペースで扱う
 (setq-default indent-tabs-mode nil)
+
 ;; タブ幅
 (custom-set-variables '(tab-width 4))
+
 ;; フォント設定
 (set-face-attribute 'default nil
                     :family "Ricty Diminished Discord"
