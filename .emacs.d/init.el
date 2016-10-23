2461a6c1f64617971451dc99df0578fae8151719
mochi8k <y.hkh.185@gmail.com>
2016-10-23 13:40:24 +0900

    add setting of auto-complete

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -122,3 +122,8 @@
       (eval-print-last-sexp))))
 
 (el-get 'sync)
+
+;; auto-complete
+(el-get-bundle auto-complete)
+(when (require 'auto-complete nil t)
+  (setq ac-comphist-file "~/.emacs.d/cache/auto-complete/ac-comphist.dat"))
