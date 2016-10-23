885d14c30df413f4391753b1ad305c94049a153d
mochi8k <y.hkh.185@gmail.com>
2016-10-23 13:40:49 +0900

    add setting of anything

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -127,3 +127,7 @@
 (el-get-bundle auto-complete)
 (when (require 'auto-complete nil t)
   (setq ac-comphist-file "~/.emacs.d/cache/auto-complete/ac-comphist.dat"))
+
+;; anything
+(el-get-bundle anything)
+(when (require 'anything nil t))
