f8ec687ae6a77791900a91bcb79f45efbc5d3f73
mochi8k <y.hkh.185@gmail.com>
2016-10-23 13:39:03 +0900

    hello el-get

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -107,3 +107,18 @@
     (turn-on-eldoc-mode)))
 
 (add-hook 'emacs-lisp-mode-hook 'elisp-mode-hooks)
+
+
+;;; el-get
+
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (let (el-get-master-branch)
+      (goto-char (point-max))
+      (eval-print-last-sexp))))
+
+(el-get 'sync)
