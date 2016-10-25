ae3f36546df05367e58ed959c74d32765d378fb7
mochi8k <y.hkh.185@gmail.com>
2016-10-26 07:57:04 +0900

    [moccur] add setting

--- .emacs.d/inits/10_color-moccur.el
+++ .emacs.d/inits/10_color-moccur.el
@@ -1,1 +1,12 @@
-(when (require 'color-moccur nil t))
+(when (require 'color-moccur nil t)
+  ;; M-oにoccur-by-moccurを割り当て
+  (define-key global-map (kbd "M-o") 'occur-by-moccur)
+  ;; スペース区切りでAND検索
+  (setq moccur-split-word t)
+  ;; ディレクトリ検索のとき除外するファイル
+  (add-to-list 'dmoccur-exclusion-mask "\\.DS_Store")
+  (add-to-list 'dmoccur-exclusion-mask "^#.+#$")
+  ;; Migemoを利用できる環境であればMigemoを使う
+  (when (and (executable-find "cmigemo")
+             (require 'migemo nil t))
+    (setq moccur-use-migemo t)))
