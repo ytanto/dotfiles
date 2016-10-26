e5dcd635f1936ca69a2bba9180cb93c3f6dcd180
mochi8k <y.hkh.185@gmail.com>
2016-10-27 00:42:02 +0900

    [Go] update 50_Go.el

--- .emacs.d/inits/50_Go.el
+++ .emacs.d/inits/50_Go.el
@@ -1,4 +1,13 @@
+(when (require 'exec-path-from-shell)
+  (let ((envs '("GOROOT" "GOPATH")))
+    (exec-path-from-shell-copy-envs envs)))
+
 (require 'go-mode nil t)
 (require 'go-autocomplete nil t)
 (require 'go-eldoc nil t)
-(add-hook 'go-mode-hook '(lambda () (setq tab-width 2)))
+(add-hook 'go-mode-hook
+          (lambda ()
+            (setq tab-width 2)
+            (go-eldoc-setup)
+            (add-hook 'before-save-hook 'gofmt-before-save)
+            ))
