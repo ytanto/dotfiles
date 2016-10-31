1341f61d1bf1daf3844632955dc8773ab84071db
mochi8k <y.hkh.185@gmail.com>
2016-11-01 01:20:19 +0900

    [helm] update 10_helm.el

--- .emacs.d/inits/10_helm.el
+++ .emacs.d/inits/10_helm.el
@@ -1,3 +1,13 @@
 (when (require 'helm-config)
+  ;; helm-for-files
   (define-key global-map (kbd "C-x b") 'helm-for-files)
-  (define-key global-map (kbd "C-x C-f") 'helm-find-files))
+  
+  ;; helm-find-files
+  (define-key global-map (kbd "C-x C-f") 'helm-find-files)
+
+  ;; tab 補完
+  (define-key helm-find-files-map (kbd "<tab>") 'helm-execute-persistent-action)
+
+  ;; C-h バックスペース
+  (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char))
+
