7ad3c6faa300203f0343f39b3a680f43ec9993e9
mochi8k <y.hkh.185@gmail.com>
2016-11-03 23:10:19 +0900

    [JavaScript] modofy extension

--- .emacs.d/inits/50_JavaScript.el
+++ .emacs.d/inits/50_JavaScript.el
@@ -1,8 +1,8 @@
 (when (require 'js2-mode)
-  (add-to-list 'auto-mode-alist '("\\.js" . js2-mode))
-  (add-to-list 'auto-mode-alist '("\\.es" . js2-mode))
-  (add-to-list 'auto-mode-alist '("\\.es6" . js2-mode))
-  (add-to-list 'auto-mode-alist '("\\.jsx" . js2-mode))
+  (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+  (add-to-list 'auto-mode-alist '("\\.es$" . js2-mode))
+  (add-to-list 'auto-mode-alist '("\\.es6$" . js2-mode))
+  (add-to-list 'auto-mode-alist '("\\.jsx$" . js2-mode))
 
   ;; js2-modeのインデントの不具合を解決
   (add-hook 'js2-mode-hook 'js-indent-hook)
