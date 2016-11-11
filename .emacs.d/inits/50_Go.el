20dc832d8810f05dd1e912ea643c3d23f2ee866e
mochi8k <y.hkh.185@gmail.com>
2016-11-12 08:40:08 +0900

    [Go] update setting

--- .emacs.d/inits/50_Go.el
+++ .emacs.d/inits/50_Go.el
@@ -5,6 +5,7 @@
 (require 'go-mode nil t)
 (require 'go-autocomplete nil t)
 (require 'go-eldoc nil t)
+(add-hook 'go-mode-hook 'flycheck-mode)
 (add-hook 'go-mode-hook
           (lambda ()
             (setq tab-width 2)
