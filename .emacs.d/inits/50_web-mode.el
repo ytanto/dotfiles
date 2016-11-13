d5f3c4051fd1509bcf5563bd6b616356f75dae39
mochi8k <y.hkh.185@gmail.com>
2016-11-14 01:01:18 +0900

    [web-mode] update

--- .emacs.d/inits/50_web-mode.el
+++ .emacs.d/inits/50_web-mode.el
@@ -1,9 +1,10 @@
 (when (require 'web-mode)
   (add-to-list 'auto-mode-alist '("\\.tpl$" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
 
   (defun web-mode-hook()
     (setq web-mode-markup-indent-offset 2)
     (setq web-mode-css-indent-offset 2)
-    (setq web-mode-code-indent-offset 2))
-
+    (setq web-mode-code-indent-offset 2)
+    (setq indent-tabs-mode t))
   (add-hook 'web-mode-hook 'web-mode-hook))
