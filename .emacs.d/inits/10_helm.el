171afd7ce3f647745cf016b7d2a952010467d60c
mochi8k <y.hkh.185@gmail.com>
2017-04-03 00:13:04 +0900

    [helm] helm-source-ls-git-status

--- .emacs.d/inits/10_helm.el
+++ .emacs.d/inits/10_helm.el
@@ -16,6 +16,7 @@
      '(helm-truncate-lines t)
      '(helm-delete-minibuffer-contents-from-point t)
      '(helm-mini-default-sources '(helm-source-buffers-list
+                                   helm-source-ls-git-status
                                    helm-source-files-in-current-dir
                                    helm-source-ls-git
                                    helm-source-recentf)))
