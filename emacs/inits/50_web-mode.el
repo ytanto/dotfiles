7f8f434586aafc59aea0f73b2be029af508499a6
mochi8k <y.hkh.185@gmail.com>
2017-05-13 20:00:15 +0900

    [Emacs] update 50_web-mode.el

--- emacs/inits/50_web-mode.el
+++ emacs/inits/50_web-mode.el
@@ -1,6 +1,7 @@
 (when (require 'web-mode)
   (add-to-list 'auto-mode-alist '("\\.tpl$" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.html.erb$" . web-mode))
 
   (defun web-mode-hook()
     (setq web-mode-markup-indent-offset 2)
