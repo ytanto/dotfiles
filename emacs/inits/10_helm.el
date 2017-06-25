0d40685a7c6384d189714fc287557ee55f7ff7b4
mochi8k <y.hkh.185@gmail.com>
2017-06-25 13:00:34 +0900

    [Emacs] global-set-key for helm-imenu

--- emacs/inits/10_helm.el
+++ emacs/inits/10_helm.el
@@ -5,6 +5,7 @@
   ;; helm-find-files
   (define-key global-map (kbd "C-x C-f") 'helm-find-files)
 
+
   ;; tab 補完
   (define-key helm-find-files-map (kbd "<tab>") 'helm-execute-persistent-action)
 
@@ -12,6 +13,8 @@
   (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
 
   (global-set-key (kbd "M-x") 'helm-M-x)
+
+  (global-set-key (kbd "C-x C-r") 'helm-imenu)
 
   (when (require 'helm-ls-git)
     (custom-set-variables
