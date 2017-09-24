a19293d8b44d38e7ef62d7a174ae365a69103267
mochi8k <y.hkh.185@gmail.com>
2017-09-25 08:10:01 +0900

    [Emacs] port file

--- emacs/inits/50_JavaScript.el
+++ emacs/inits/50_JavaScript.el
@@ -12,7 +12,10 @@
     "Return depth attribute for CANDIDATE. 'nil' entries are treated as 0."
     (let ((depth (get-text-property 0 'depth candidate)))
       (if (eq depth nil) 0 depth)))
-  (add-hook 'js2-mode-hook 'tern-mode)
+  (add-hook 'js2-mode-hook
+            '(lambda ()
+               (setq tern-command '("tern" "--no-port-file"))
+               (tern-mode t)))
   (add-to-list 'company-backends 'company-tern)
 
 
