8646b9e52f5cda481c6ec041561eab6a6d1d8d58
mochi8k <y.hkh.185@gmail.com>
2017-09-25 08:10:23 +0900

    [Emacs] eslint for web-mode

--- emacs/inits/50_web-mode.el
+++ emacs/inits/50_web-mode.el
@@ -8,4 +8,7 @@
     (setq web-mode-markup-indent-offset 4)
     (setq web-mode-css-indent-offset 4)
     (setq web-mode-code-indent-offset 4)
+    (lambda ()
+      (flycheck-add-mode 'javascript-eslint 'web-mode)
+      (flycheck-mode))
   (add-hook 'web-mode-hook 'web-mode-hook)))
