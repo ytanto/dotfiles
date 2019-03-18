4e073028af578d5bf5b5ae3e64cb7b57b4c74505
ytanto <y.hkh.185@gmail.com>
2019-03-18 18:59:20 +0900

    [Emacs] add 10_neotree.el

---
+++ emacs/inits/10_neotree.el
@@ -0,0 +1,7 @@
+(require 'neotree)
+(setq neo-show-hidden-files t)
+(setq neo-smart-open nil)
+
+;; delete-other-window で neotree ウィンドウを消さない
+(setq neo-persist-show t)
+;; (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
